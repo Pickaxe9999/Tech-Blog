e1b506a2c23d69e224e0b0f2e07fd17cf8a0c97d
Jacob H <jacob.hannah164@gmail.com>
2022-08-16 20:32:57 -0400

    created comment and post model

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+onst { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//comment table
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,62 @@
 const User = require('./User.js');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+/*
+A User can have many Posts but
+a post only belongs to a single user however
+a user can leave many ratings on many posts while
+a post can have many ratings from many users
+*/
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+})
+
+User.belongsToMany(Post, {
+    through: 'rate',
+    as: 'rating',
+
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+})
+
+Post.belongsToMany(User, {
+    through: 'rate',
+    as: 'rating',
+
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+})
+
+
+
+/*
+Comments are given by users,
+Comment are given to posts
+*/
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+})
 
 module.exports = {User};
